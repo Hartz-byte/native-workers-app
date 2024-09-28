85535cfb460ebb6352f7483cb3d531c2c8c1ae77
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-28 13:13:02 +0530

    all screens styled

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Chat = () => {
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      {/* top icons and logo */}
+      
+    </SafeAreaView>
   );
 };
 
 export default Chat;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,7 @@
-// import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-// import * as SplashScreen from "expo-splash-screen";
-// import { useEffect } from "react";
 import { StatusBar } from "react-native";
 
-// SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  // font load
-  // const [loaded] = useFonts({
-  //   SFPRODISPLAYREGULAR: require("../assets/fonts/SFPRODISPLAYREGULAR.ttf"),
-  //   SFPRODISPLAYMEDIUM: require("../assets/fonts/SFPRODISPLAYMEDIUM.ttf"),
-  //   SFPRODISPLAYSEMIBOLDITALIC: require("../assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.ttf"),
-  //   SFPRODISPLAYBOLD: require("../assets/fonts/SFPRODISPLAYBOLD.ttf"),
-  //   SFPRODISPLAYHEAVYITALIC: require("../assets/fonts/SFPRODISPLAYHEAVYITALIC.ttf"),
-  //   SFPRODISPLAYBLACKITALIC: require("../assets/fonts/SFPRODISPLAYBLACKITALIC.ttf"),
-  //   "Lato-Bold": require("../assets/fonts/Lato-Bold.ttf"),
-  // });
-
-  // useEffect(() => {
-  //   if (loaded) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded]);
-
-  // if (!loaded) {
-  //   return null;
-  // }
-
   return (
     <>
       {/* statusbar */}
